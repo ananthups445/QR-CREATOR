3d29520294ee8ebc988d6a2471b4988040e9f233
Ananth <ananthups445@gmail.com>
2026-05-06 23:16:43 +0530

    Fix blank page - add basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/QR-CREATOR">
       <App />
     </BrowserRouter>
   </React.StrictMode>
